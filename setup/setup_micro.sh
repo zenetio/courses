6ea5cbb694e75a6f8858e4ba9b6bda4e1391c1e2
Carlos Roberto <carlosrl@gmail.com>
2017-01-17 18:08:43 +0000

    Update aws-alias.sh and setup_p2.sh

--- setup/setup_micro.sh
+++ setup/setup_micro.sh
@@ -7,14 +7,14 @@ export cidr="0.0.0.0/0"
 
 # get the correct ami
 export region=`aws configure get region`
-if [ $region = "us-west-2" ]; then 
+if [ $region = "us-west-2" ]; then
    export ami="ami-9c54f4fc" # Oregon
-elif [ $region = "eu-west-1" ]; then 
-   export ami="ami-b43d1ec7" # Ireland
+elif [ $region = "eu-west-1" ]; then
+   export ami="ami-9e1a35ed" # Ireland
 elif [ $region = "us-east-1" ]; then
-  export ami="ami-31ecfb26" # Virginia
+  export ami="ami-9c5b438b" # Virginia
 else
-  echo "Only us-west-2 (Oregon), eu-west-1 (Ireland) and us-east-1 (Virginia) are currently supported"
+  echo "Only us-west-2 (Oregon), eu-west-1 (Ireland), and us-east-1 (Virginia) are currently supported"
   exit 1
 fi
 
@@ -52,7 +52,7 @@ then
 	chmod 400 ~/.ssh/aws-key-$name.pem
 fi
 
-export instanceId=`aws ec2 run-instances --image-id $ami --count 1 --instance-type p2.xlarge --key-name aws-key-$name --security-group-ids $securityGroupId --subnet-id $subnetId --associate-public-ip-address --block-device-mapping "[ { \"DeviceName\": \"/dev/sda1\", \"Ebs\": { \"VolumeSize\": 30, \"VolumeType\": \"gp2\" } } ]" --query 'Instances[0].InstanceId' --output text`
+export instanceId=`aws ec2 run-instances --image-id $ami --count 1 --instance-type t2.micro --key-name aws-key-$name --security-group-ids $securityGroupId --subnet-id $subnetId --associate-public-ip-address --block-device-mapping "[ { \"DeviceName\": \"/dev/sda1\", \"Ebs\": { \"VolumeSize\": 30, \"VolumeType\": \"gp2\" } } ]" --query 'Instances[0].InstanceId' --output text`
 aws ec2 create-tags --resources $instanceId --tags --tags Key=Name,Value=$name-gpu-machine
 export allocAddr=`aws ec2 allocate-address --domain vpc --query 'AllocationId' --output text`
 
